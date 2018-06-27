a3a8465d7e7c0bd8af92215f7337f0fa2f024871
qtabs <alextabas@gmail.com>
2018-06-27 16:34:34 +0200

    Initial commit

---
+++ taglabration.py
@@ -0,0 +1,159 @@
+import pyaudio
+import numpy as np
+import taglameter
+
+def calibrate():
+
+    """ 
+    Main function for the recalibration procedure. After the recalibration is 
+    finished the paramters are saved in an npz file specified in 
+    loadCalibrationParameters(). A previous calibration is required.
+    
+    * Calibration functions should be moved to another library. Common 
+    functions to calibrate() and measureThreshold() should be moved to a core
+    library. <-- ToDo. 
+    * A calibration initialisation should be created if no previous 
+    calibration exists. <-- ToDo.
+
+    Inputs
+        port : pyAudio port to stream the sound
+    """
+
+    import shutil
+
+    port = pyaudio.PyAudio() 
+
+    print "\n\nWARNING! You are recalibrating the system! ",
+    answ = raw_input("Is that what you really want to do?! ")
+    
+    if not(answ.lower() in ['y', 'yes', 'si', 'oui', 'ja', 'jaaaa']):
+        print "Ok, no worries! Leaving calibration function..."
+        return()
+
+    [calFile, freq, loud, prevA0, eps, par] = loadCalibrationParameters()
+    A0 = np.zeros(prevA0.shape)
+
+    for fIx, f in enumerate(freq):
+        for lIx, l in enumerate(loud):
+            print "\n\nf = {:.0f}Hz, target loudness = {:.1f}dB".format(f, l)
+            print "  | To increase intensity, press: up, right, w, s, x"
+            print "  | To decrease intensity, press: down, left, q, a, z"
+            print "  | To accept calibration, press: ENTER"
+            A0[fIx,lIx] = calibrateTone(f, l, prevA0[fIx,lIx], port, par, eps)
+
+    shutil.move(calFile, calFile[0:-4]+ 'Prev' + calFile[-4:])
+    np.savez(calFile, A0 = A0, FREQ = freq, LOUD = loud, FS = par['fs'])
+
+    print "\n\nCalibration finished! <3 File saved to {}\n\n".format(calFile)
+
+    return()
+
+
+
+def calibrateTone(f, l, a0, port, par, epsilon):
+
+    """ 
+    Calibrates the waveform amplitude of a [frequency, loudness] pair.
+
+    Inputs
+        f       : pure tone frequency (Hz)
+        l       : target loudness (dB)
+        a0      : initalisation value for the waveform amplitude (0 < a0 < 1)
+        port    : pyAudio port to stream the sound
+        par     : dictionary with the parameters, check loadParameters()
+        epsilon : dictionary mapping pressed keys to adjustment sizes 
+    
+    Outputs 
+        a0      : adjusted waveform amplitude (0 <= a0 <= 1)
+    """
+
+    global lastFrame
+    fs  = float(par['fs'])
+    dur = par['dur']
+    key = None
+
+    while not (key in ['\n', ' ']):
+
+        lastFrame = 0
+
+        sound  = taglameter.createTone(f, a0, par)
+        stream = port.open(format          = pyaudio.paFloat32,
+                           channels        = 1,
+                           rate            = par['fs'],
+                           output          = True, 
+                           stream_callback = lambda i, f, t, s: 
+                                      taglameter.callback(i, f, t, s, sound))
+        
+        stream.start_stream()
+        
+        key = taglameter.listenKeyPress(par['dur'], 
+                             terminateOnPress = True, verbose = False)   
+
+        stream.stop_stream()
+        stream.close()
+
+        if key in epsilon.keys():
+            a0 = a0 + epsilon[key]
+            if a0 > 0.999:
+                a0 = 0.999
+                print "WARNING: You are trying to increase a0 but a0 < 1"
+            elif a0 < 0.001:
+                a0 = 0.001
+                print "WARNING: You are trying to decrease a0 but a0 > 0"
+            print "New factor: {:.3} ".format(a0),
+            print "(epsilon = {:.3})".format(epsilon[key])
+
+    return a0
+
+
+
+def loadCalibrationParameters():
+
+    """ 
+    Returns the calibration parameters. Change this function to adjust them.
+    
+    Outputs 
+        calf    : path to the calibration npz file 
+        freq    : array holding the considered frequency values in Hz
+        loud    : array holding the considered loundess values in dB
+        freq    : flag that marks if the sound waveform is deplected
+        prevA0  : list of arrays with the previous calibration of the system
+        epsilon : dictionary mapping pressed keys to adjustment sizes 
+        prevA0  : flag that marks if the sound waveform is deplected
+        par     : dictionary with the basic parameters of the sounds
+    """
+
+    par  = {'dur'  : 100,   # maximum duration of the sound (seconds)
+            'fs'   : 48000, # sample rate Hz
+            'tau'  : 0}     # ramps time windows
+
+    calf = './calibration.npz'   # path to calibration file
+
+    epsilon = {'\x1b[A': 0.1,    # keyboard map for the calibration
+               '\x1b[C': 0.05,   # procedure
+               'w':      0.01,
+               's':      0.005,
+               'x':      0.001,
+               'z':     -0.001,
+               'a':     -0.005,
+               'q':     -0.01,
+               '\x1b[D':-0.05, 
+               '\x1b[B':-0.1}
+
+
+    calFile = np.load(calf)
+    loud    = calFile['LOUD']
+    freq    = calFile['FREQ']
+    prevA0  = calFile['A0']
+
+    return(calf, freq, loud, prevA0, epsilon, par)
+
+
+
+
+
+
+if __name__ == "__main__":
+    
+    calibrate()
+
---
+++ taglameter.py
@@ -0,0 +1,385 @@
+import pyaudio
+import numpy as np
+import time
+import os
+import matplotlib.pyplot as plt
+lastFrame = 0 # Global variable needed for the callback. 
+              
+              # * Workaround this superugly hack:  
+              # create a Streaming object with a "callback()" method and a 
+              # lastFrame internal variable. <-- ToDo.
+
+
+def main(subID = ''):
+
+    port = pyaudio.PyAudio()  # Opens a pyAudio streaming port
+    par  = loadParameters()   # Loads the audiometer parameters
+
+    print "\n    ---------------------------------------------------"
+    print   "    | Welcome to the incredible TaboGladdoAudioMeter! |"
+    print   "    ---------------------------------------------------\n\n"
+
+    # Subject ID can be intered as an argument of this function. If not, 
+    # the ID is requested via keyboard input
+    if subID == '':
+        subID = raw_input('Enter participants ID: ')
+        if subID.lower() in ["falken", "stephen falken"]:
+            print "Greetings, Professor Falken."
+
+    # Calibration is triggered through the subject ID, we need to change this
+    if subID.lower() == 'calibrateplease':
+        calibrate(port)
+        return()   
+
+    print "Loading...."
+    time.sleep(1)
+    print "\n"
+
+    threshold = np.zeros(len(par['freq']))
+
+    # Thresholding is done sequentially for each frequency in par['freq']
+    for fIx, freq in enumerate(par['freq']):
+        print 'Testing f = {:.0f}Hz'.format(freq)
+        threshold[fIx] = measureThreshold(fIx, port, par)
+        print '---- done! Threshold at {:.1f} dB \n'.format(threshold[fIx])
+
+    saveThresholds(threshold, subID, par)
+    port.terminate()
+
+    # * Add plots of the thresholds here? It would be better to print the 
+    # plots using plain text so that the audiometer can run completely 
+    # without a window manager. Also: it would be fun to implement. <-- ToDo.
+
+    raw_input('Press ENTER to exit...')
+
+
+
+def saveThresholds(threshold, subID, par):
+
+    """ 
+    Saves the hearing thresholds in a plain textfile.
+
+    Inputs
+        threshold : array with the measured thresholds (dB). Each value 
+                    corresponds to a frequency value in par['freq']
+        subID     : subject ID
+        par       : dictionary with the parameters, check loadParameters()
+    
+    """
+
+
+    header = "Audiometry for subject {}".format(subID)
+    footer = "\n// Powered by the TaboGladdoAudioMeter!"
+    headLine = "  |".join(["{:>7.0f}".format(f) for f in par['freq']])
+    resLine  = "  |".join(["{:>7.1f}".format(t) for t in threshold])
+
+    headLine = "Freq   (Hz)  |" + headLine
+    resLine  = "Thresh (dB)  |" + resLine
+    header   = header + "\n" + "-" * len(headLine) + "\n"
+    footer   = "\n" + "-" * len(headLine) + "\n" + footer
+
+    if not os.path.isdir('./out/'):
+        os.makedirs('./out/')
+
+    resFile = "./out/audioThresh-{}".format(subID)
+    f = open(resFile, "w")
+    f.write(header + headLine + "\n" + resLine + footer)
+    f.close()
+
+    print "Audiometry completed! Results saved to {}\n".format(resFile)
+    print header + headLine + "\n" + resLine + footer
+    print "\n\n"
+
+    # * It would be cool to implement a function that automatically reads the 
+    # textfile and stores the thresholds in an array or list. <-- ToDo.
+
+
+
+def measureThreshold(fIx, port, par):
+
+    """ 
+    Runs the algorithm to measure the hearing threshold of a given frequency.
+
+    Inputs
+        fIx  : integer such that par['freq'][fIx] = tested frequency (Hz)
+        port : pyAudio port to stream the sound
+        par  : dictionary with the parameters, check loadParameters()
+    
+    Outputs 
+        key  : key pressed, None if no key is pressed.
+    """
+
+    freq     = par['freq'][fIx] # fIx : current frequency = par['freq'][lIx]
+    lIx      = par['l0Ix']      # lIx : current loudness  = par['loud'][lIx]
+
+    prev = True  # Flags if the sound was heared in the previous iteration
+    while True:
+        # Wait for a normally distributed amount of time
+        time.sleep(par['avgT'] + par['sgmT'] * abs(np.random.randn()))
+        print "     -> loudness = {:>5.1f}dB".format(par['loud'][lIx]),
+        key = playTone(freq, par['A0'][fIx][lIx], port, par)
+        print ""
+        if key is not None:
+            if prev:
+                if lIx != 0: # if loudness index is not at min level
+                    lIx = max(0, lIx - 2) # two down, with saturation at 0
+                    prev = True
+                else: # if loudness is at minimum, return minimum
+                    return par['loud'][lIx]
+            else:
+                return par['loud'][lIx]
+        else:
+            lIx += 1 # one up
+            prev = False
+            if lIx >= len(par['loud']): # if loudness index is at max level
+                return par['loud'][lIx] # return minimum
+
+
+
+def playTone(f, a0, port, par):
+
+    """ 
+    Plays a pure tone and waits for a key press through a limited amount of
+    time. Sampling rate, duration, and waiting time are parameters stored in
+    par.
+
+    Inputs
+        f    : pure tone frequency (Hz)
+        a0   : waveform amplitude (0 < a0 < 1)
+        port : pyAudio port to stream the sound
+        par  : dictionary with the parameters, check loadParameters()
+    
+    Outputs 
+        key  : key pressed, None if no key is pressed.
+    """
+
+    global lastFrame
+    lastFrame = 0
+
+    sound  = createTone(f, a0, par)
+    stream = port.open(format          = pyaudio.paFloat32,
+                       channels        = 1,
+                       rate            = par['fs'],
+                       output          = True, 
+                       stream_callback = lambda i, f, t, s: 
+                                                callback(i, f, t, s, sound))
+
+    stream.start_stream()
+    key = listenKeyPress(par['dur'] + 2 * par['tau'] + par['wait'])   
+
+    stream.stop_stream()
+    stream.close()
+
+    return(key)
+
+
+
+def listenKeyPress(waitTime, terminateOnPress = False, verbose = True):
+
+    """ 
+    Listens keyboard keypress actions for a specified amount of time. 
+    
+    * Mouse inputs should be integrated here (nightmare alert!) <-- ToDo
+
+    Inputs
+        waitTime          : maximum waiting time (seconds)
+        aterminateOnPress : function exits on key press if set to True
+        verbosity         : verbose flag (True/False)
+    
+    Outputs 
+        key               : first pressed key, None if no key is pressed
+    """
+
+    import termios, fcntl, sys, os
+    fd = sys.stdin.fileno()
+
+    oldterm = termios.tcgetattr(fd)
+    newattr = termios.tcgetattr(fd)
+    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+    termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+    oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+    noKeyPresed = True
+    key         = None
+    startTime   = time.time()
+    try:
+        while (time.time() - startTime) < waitTime and noKeyPresed:
+            try:
+                key = sys.stdin.read()
+                noKeyPresed = True
+                if verbose:
+                    print ' - response! (key pressed = {})'.format(key),
+                break
+            except IOError: pass
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+
+    if not(terminateOnPress) and (time.time() - startTime) < waitTime:
+        time.sleep(waitTime - time.time() + startTime)
+
+    return(key)
+
+
+
+def createTone(f, a0, par):
+
+    """ 
+    Creates a pure tone. 
+    
+    * It would be better to generate the tone online. Adding the ramps might
+    be challenging but it should be possible with a phase internal variable 
+    if the function is added as a method to the Streaming object <-- ToDo.
+
+    Inputs
+        f     : pure tone frequency (Hz)
+        a0    : waveform amplitude (0 < a0 < 1)
+        par   : dictionary specifyint the duration, sampling rate, and hamming
+                window ramp/damp size; check loadParameters()
+    
+    Outputs 
+        sound : sound waveform
+    """
+
+    x     = np.linspace(0, par['dur'], int(par['dur'] * par['fs']));
+    omega = 2 * np.pi * f
+    sound = (a0 * np.sin(omega * x)).astype(np.float32)
+
+    if par['tau'] > 0:
+        hL = int(par['tau'] * par['fs'])
+        hw = np.hamming(2 * hL)
+        sound[:hL ] = sound[:hL ] * hw[:hL]
+        sound[-hL:] = sound[-hL:] * hw[hL:]
+
+        sound = (np.concatenate((np.zeros((hL,)), 
+                                 sound, 
+                                 np.zeros((hL,))))).astype(np.float32)
+
+    return sound
+
+
+
+def callback(in_data, frame_count, time_info, status, sound):
+
+    """ 
+    Callback function for pyAudio stream. Returns a chunk of the provided 
+    sound. Needs a "lastFrame" variable defined globally to keep track of 
+    the position within the sound.
+    
+    Inputs
+        in_data     : arg required by pyAudio but not in use
+        frame_count : chunk size (integer)
+        time_info   : arg required by pyAudio but not in use
+        status      : arg required by pyAudio but not in use
+        sound       : waveform of the entire sound
+    
+    Outputs 
+        chunk       : excerpt of the sound waveform
+        finished    : flag that marks if the sound waveform is deplected
+    """
+
+    global lastFrame
+
+    prevFrame = lastFrame
+    lastFrame = prevFrame + frame_count
+    
+    if lastFrame >= len(sound):
+        finished = True
+        lastFrame = len(sound)
+    else:
+        finished = False
+
+    chunk = sound[prevFrame:lastFrame]
+
+    return (chunk, finished)
+
+
+
+def loadCalibrationParameters():
+
+    """ 
+    Returns the calibration parameters. Change this function to adjust them.
+    
+    Outputs 
+        calf    : path to the calibration npz file 
+        freq    : array holding the considered frequency values in Hz
+        loud    : array holding the considered loundess values in dB
+        freq    : flag that marks if the sound waveform is deplected
+        prevA0  : list of arrays with the previous calibration of the system
+        epsilon : dictionary mapping pressed keys to adjustment sizes 
+        prevA0  : flag that marks if the sound waveform is deplected
+        par     : dictionary with the basic parameters of the sounds
+    """
+
+    par  = {'dur'  : 100,   # maximum duration of the sound (seconds)
+            'fs'   : 48000, # sample rate Hz
+            'tau'  : 0}     # ramps time windows
+
+    calf = './calibration.npz'   # path to calibration file
+
+    epsilon = {'\x1b[A': 0.1,    # keyboard map for the calibration
+               '\x1b[C': 0.05,   # procedure
+               'w':      0.01,
+               's':      0.005,
+               'x':      0.001,
+               'z':     -0.001,
+               'a':     -0.005,
+               'q':     -0.01,
+               '\x1b[D':-0.05, 
+               '\x1b[B':-0.1}
+
+
+    calFile = np.load(calf)
+    loud    = calFile['LOUD']
+    freq    = calFile['FREQ']
+    prevA0  = calFile['A0']
+
+    return(calf, freq, loud, prevA0, epsilon, par)
+
+
+
+def loadParameters():
+
+    """ 
+    Returns the audiometer parameters. All frequencies and the initial 
+    louness must belong to the values stored in the calibration file.
+    """
+
+    # Audiometer parameters -- Feel free to mess around! :)
+    par = {'dur'  : 1.1,   # tone duration including ramps (seconds)
+           'fs'   : 48000, # sample rate Hz
+           'tau'  : 0.05,  # ramps time windows
+           'avgT' : 1,     # average pause time between tones (seconds)
+           'sgmT' : 1,     # std of pause time between tones (seconds)
+           'wait' : 1,     # response waiting time after offset (seconds)
+           'l0'   : 30,    # initial loudness for thresholding (dB SPL)
+           'freq' : [.25, .5, 1, 2, 3, 4, 6, 8, 10, 12, 14, 16, 20], # kHz
+           'calf' : './calibration.npz' # path to calibration file
+           }
+
+
+    # Transforms --- touching this can be punished with death (by spoon!)
+    par['freq'] = np.array(par['freq']) * 1000  
+
+    calFile     = np.load(par['calf'])
+    par['loud'] = calFile['LOUD']
+    par['l0Ix'] = [ix for ix, loud in enumerate(par['loud']) 
+                                                    if loud == par['l0']][0]
+    par['A0']   = [calFile['A0'][ix] for ix in [ix 
+                                      for ix, F in enumerate(calFile['FREQ']) 
+                                      for f in par['freq'] if F==f]]
+
+    if par['fs'] != calFile['FS']:
+        print "WARNING! Sample rate set to {}Hz ".format(par['fs']),
+        print "but calibration was performed at {}Hz".format(calFile['FS'])
+
+    return par
+
+
+
+if __name__ == "__main__":
+    
+    main()
+
